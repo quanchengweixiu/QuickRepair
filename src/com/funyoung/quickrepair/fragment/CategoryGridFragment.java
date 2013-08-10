fcb15729103f11670e2a5fb01e5e1f9eae3066b7
YangFeng <e13310@gmail.com>
2013-08-11 00:31:59 +0800

    On branch dev-sherlock-navigation-drawer  Changes to be committed: apply category image.

--- src/com/funyoung/quickrepair/fragment/CategoryGridFragment.java
+++ src/com/funyoung/quickrepair/fragment/CategoryGridFragment.java
@@ -5,8 +5,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 
 import baidumapsdk.demo.R;
@@ -19,30 +25,51 @@ import baidumapsdk.demo.R;
  * Fragment that appears in the "content_frame", shows a planet
  */
 public  class CategoryGridFragment extends Fragment {
-    public static final String ARG_PLANET_NUMBER = "planet_number";
-
     private View rootView;
 
     public CategoryGridFragment() {
         // Empty constructor required for fragment subclasses
     }
 
+    private static Integer[] images = {
+            R.drawable.ic_classify_airconditioning,
+            R.drawable.ic_classify_heater,
+            R.drawable.ic_classify_closestool,
+            R.drawable.ic_classify_electric,
+            R.drawable.ic_classify_pipeline,
+            R.drawable.ic_classify_appliance,
+            R.drawable.ic_classify_house,
+            R.drawable.ic_classify_furniture,
+            R.drawable.ic_classify_more
+    };
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.fragment_planet, container, false);
-        int i = getArguments().getInt(ARG_PLANET_NUMBER);
-        update(i);
+        rootView = inflater.inflate(R.layout.fragment_category, container, false);
+
+        ArrayList<HashMap<String, Object>> itemData = new ArrayList<HashMap<String, Object>>();
+        final String[] labels = getResources().getStringArray(R.array.qp_category_label_array);
+        for (int i = 0; i < 9; i++) {
+            HashMap<String, Object> itemUnit = new HashMap<String, Object>();
+            itemUnit.put("img", images[i]);
+            itemUnit.put("label", labels[i]);
+            itemData.add(itemUnit);
+        }
+
+        GridView gridView = (GridView) rootView.findViewById(R.id.ptr_gridview);
+//        ListAdapter adapter = new ArrayAdapter<String>(getActivity(),
+//                R.layout.gridview_item_category,
+//                labels);
+        SimpleAdapter adapter = new SimpleAdapter(getActivity(),
+                itemData,
+                R.layout.gridview_item_category,
+                new String[] { "img", "label" },
+                new int[] { R.id.img, R.id.label });
+
+        gridView.setAdapter(adapter);
+
         return rootView;
-    }
-
-    public void update(int position) {
-        String planet = getResources().getStringArray(R.array.planets_array)[position];
-
-        int imageId = getResources().getIdentifier(planet.toLowerCase(Locale.getDefault()),
-                "drawable", getActivity().getPackageName());
-        ((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
-        getActivity().setTitle(planet);
     }
 }
 
